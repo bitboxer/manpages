37ea51d4e1b25527d66f9dd95a8532f8ed85f55c
Bodo Tasche <bodo@wannawork.de>
2016-10-29 08:22:28 +0100

    [#20] Using wrong directory to save man pages (#21)

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -5,7 +5,7 @@ Gem::CommandManager.instance.register_command(:manpages)
 
 Gem.post_install do |installer|
   source_dir = installer.spec.gem_dir
-  target_dir = File.expand_path(installer.bin_dir, "../share/man")
+  target_dir = File.expand_path("#{installer.bin_dir}/../share/man")
 
   Manpages::Install.new(installer.spec, source_dir, target_dir).install_manpages
 end
@@ -13,7 +13,7 @@ end
 Gem.pre_uninstall do |uninstaller|
   bin_dir = uninstaller.bin_dir || Gem.bindir(uninstaller.spec.base_dir)
   source_dir = uninstaller.spec.gem_dir
-  target_dir = File.expand_path(bin_dir, "../share/man")
+  target_dir = File.expand_path("#{bin_dir}/../share/man")
 
   Manpages::Uninstall.new(uninstaller.spec, source_dir, target_dir).uninstall_manpages
 end
